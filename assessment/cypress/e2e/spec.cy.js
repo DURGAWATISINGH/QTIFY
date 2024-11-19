93a4efb473682ba82c7a3ec3d216f7e60e8e88f8
CrioUser <crio-user@criodo.com>
2024-11-19 21:41:55 +0530

    ME_FE_BUILDOUT_XPROFILE_MODULE_ONE

--- assessment/cypress/e2e/spec.cy.js
+++ assessment/cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 describe('XProfile Tests', () => {
   beforeEach(() => {
     // Visits the page before each test and waits for assets to load
-    cy.visit('http://15.207.221.110:8082/') // Replace with your actual URL
+    cy.visit('http://13.126.156.151:8081/') // Replace with your actual URL
     cy.wait(2000); // Wait 2 seconds to ensure the page is fully loaded
   });
 
